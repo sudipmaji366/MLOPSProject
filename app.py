556dd2969f5b22e4cda9d2bd3229f1c05867ccc3
sudipmaji366 <sudipmaji366@gmail.com>
2025-09-04 23:54:59 +0530

    elasticbean deployment

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from src.Pipeline.predict_pipeline import CustomData,PredictPipeline
+application=Flask(__name__)
+
+app=application
+
+## Route for a home page
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+@app.route('/predictdata', methods=['GET', 'POST'])
+def predict_datapoint():
+    """
+    This function handles the prediction of student performance based on input data.
+    It processes the form data, creates a CustomData object, converts it to a DataFrame,
+    and then uses a PredictPipeline object to make predictions.
+
+    Parameters:
+    - request: The Flask request object containing form data.
+
+    Returns:
+    - A rendered HTML template with the prediction results if the request method is POST.
+    - A rendered HTML template if the request method is GET.
+    """
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+        )
+        pred_df = data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline = PredictPipeline()
+        print("Mid Prediction")
+        results = predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html', results=results[0])    
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",debug=True)                
+
+
--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 import sys
 
 def error_message_details(error, error_details:sys):
-    _,_,exc_tb=error_details.exec_info()
+    _,_,exc_tb=error_details.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
      file_name,exc_tb.tb_lineno,str(error))
